36765fe484401ec3390674cc814f761cb79694b9
vitorhmf <vitorhmf@gmail.com>
2022-08-24 12:46:03 -0300

    commit

---
+++ run_model.sh
@@ -0,0 +1,10 @@
+# variable
+date_now=(date +'%Y-%m-%dT%H:%M:%S')
+
+#data=$( date +'%Y-%m-%dT%H:%M:%S')
+
+#path
+# path='/home/vitor/Repos/loyalty-program'
+# path_to_env='/home/vitor/.pyenv/versions/3.9.0/envs/LoyaltyProgram/bin'
+
+# $path_to_env/papermill $path/scr/loyalty_program.ipynb $path/reports/loyalty_program_$data.ipynb
