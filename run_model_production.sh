bcdb24282c5630c6ec1cccd62c2ea5c5c55ef65f
vitorhmf <vitorhmf@gmail.com>
2022-08-29 16:32:18 -0300

    commit

--- run_model_production.sh
+++ run_model_production.sh
@@ -1,10 +1,8 @@
 # variable
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
-
-
 #path
-path='/home/vitor/Repos/loyalty-program'
-path_to_env='/home/vitor/.pyenv/versions/3.9.0/envs/LoyaltyProgram/bin'
+path='/home/ubunto/loyalty-program'
+path_to_env='/home/ubuntu/.pyenv/versions/3.9.0/envs/LoyaltyProgram/bin'
 
 $path_to_env/papermill $path/scr/loyalty_program.ipynb $path/reports/loyalty_program_$data.ipynb
